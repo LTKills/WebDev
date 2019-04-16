42ff07be09c83af2331b88be43e3bd8fdb899bf3
BrunoGomesCoelho <brasinhacoelho@hotmail.com>
2019-04-16 14:30:22 -0300

    "finish exercise"

--- ex6-gallery/main.js
+++ ex6-gallery/main.js
@@ -4,11 +4,37 @@ var thumbBar = document.querySelector('.thumb-bar');
 btn = document.querySelector('button');
 var overlay = document.querySelector('.overlay');
 
-/* Looping through images */
 
+/* Looping through images */
+for (var i = 1; i < 6; i++) {
   var newImage = document.createElement('img');
-  newImage.setAttribute('src', xxx);
+  newImage.setAttribute('src', "images/pic" + i + ".jpg");
   thumbBar.appendChild(newImage);
 
+  newImage.onclick = function(evnt) {
+    var imgSrc = evnt.target.getAttribute('src');
+    displayedImage.setAttribute("src", imgSrc)
+  }
+}
+
+
+
 /* Wiring up the Darken/Lighten button */
+btn.onclick = function(event) {
+  var btnClass = btn.getAttribute('class');
+
+  if (btnClass === "dark") {
+    btn.setAttribute('class', "light");
+    btn.textContent = "Lighten";
+    overlay.style.backgroundColor = "rgba(0,0,0,0.5)";
+    darkened = false
+  }
+
+  else {
+    btn.setAttribute('class', "dark");
+    btn.textContent = "Darken";
+    overlay.style.backgroundColor = "rgba(0,0,0,0)";
+    darkened = true
+  }
+}
 
