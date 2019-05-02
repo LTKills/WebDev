10a47c45655594efd066d83c89c74f27d84e4bef
Gabriel Cruz <gabs.oficial98@gmail.com>
2019-05-02 09:22:36 -0300

    Translate alert messages to portuguese

--- ex9-fetch-api/src.js
+++ ex9-fetch-api/src.js
@@ -1,14 +1,14 @@
 // Formats a Date object into the format needed for the API
 format_date = function(date_obj) {
+
     // We sum 1 since the month is zero based and since UTC makes our day 1 less
-    month = parseInt(date_obj.getMonth()) + 1 
-    day = parseInt(date_obj.getDate()) + 1 
+    month = parseInt(date_obj.getMonth()) + 1
+    day = parseInt(date_obj.getDate()) + 1
     year = date_obj.getFullYear()
     return month + "-" + day + "-" + year
 }
 
 
-// Formats the dates into the needed URL
 format_url = function(date_start, date_end) {
     str_date_start = format_date(date_start)
     str_date_end = format_date(date_end)
@@ -19,7 +19,6 @@ format_url = function(date_start, date_end) {
 
 
 
-// Return the json from a given url
 get_json = function (url) {
     var Httpreq = new XMLHttpRequest() // a new request
     Httpreq.open("GET", url)
@@ -29,36 +28,25 @@ get_json = function (url) {
 
 
 
-
-
-// Main driver function
 get_quotation = function() {
     var one_day = 24*60*60*1000; // hours*minutes*seconds*milliseconds
     var start_date = document.getElementById("startDate").value
     var end_date = document.getElementById("endDate").value
-
-    /*
-    if (start_date === "" || end_date === "") {
-	alert("Both dates must be filled in!")
-	return
-    }
-    */
+    let buyOrSell = document.getElementById("buyOrSell").value
+    let order = document.getElementById("order").value
 
     start_date = new Date(start_date)
     end_date = new Date(end_date)
     days_difference = Math.round((end_date-start_date) / one_day)
-    /*
+
     if (days_difference < 10 || days_difference > 90) {
-	alert("Dates must be between 10 and 90 days of each other!")
-	return
+        alert("Escolha um período de 10 a 90 dias")
+        return
     }
-    */
 
     url = format_url(start_date, end_date)
     json_obj = get_json(url)
-    alert(json_obj)
-    // alert(JSON.stringify(json_obj))
-    alert(url)
+    alert(JSON.stringify(json_obj))
 }
 
 
