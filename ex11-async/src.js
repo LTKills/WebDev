9f675a6c1b8f7a6e628fc30b54097ef854babbc0
BrunoGomesCoelho <brasinhacoelho@hotmail.com>
2019-05-11 13:42:06 -0300

    "Updating cars"

--- ex11-async/src.js
+++ ex11-async/src.js
@@ -6,6 +6,8 @@ let rotateCount = 0;
 let startTime = null;
 let rAF;
 
+var TESTING = false
+
 
 // Store references to the start button and the result paragraph
 const btn = document.querySelector('button');
@@ -16,6 +18,21 @@ function random(min,max) {
     // function to generate random number
     var num = Math.floor(Math.random()*(max-min)) + min;
     return num;
+}
+
+
+function moveRight(id) {
+    var pp = document.getElementById(id);
+    var right = parseInt(pp.style.left) || 0;
+
+    right += 100 // speed;  // move
+    pp.style.left = right + "px";
+    alert("Hi")
+
+    //var move = setTimeout(function() {
+    //	moveRight(id);
+    //}, 50);
+
 }
 
 
@@ -64,7 +81,11 @@ function start() {
     spinnerContainer.style.display = 'block';
     btn.style.display = 'none';
     // run the setEndgame() function after a random number of seconds between 5 and 10
-    setTimeout(setEndgame, random(5000,10000));
+    if (TESTING) {
+        setTimeout(setEndgame, random(5000,10000));
+    } else {
+        setTimeout(setEndgame, random(1000,2000));
+    }
 }
 
 
@@ -81,6 +102,7 @@ function setEndgame() {
 	console.log(e.key);
 	if(e.key === "a") {
 	    result.textContent = 'Player 1 won!!';
+	    moveRight('yellowCar');
 	} else if(e.key === "l") {
 	    result.textContent = 'Player 2 won!!';
 	}
