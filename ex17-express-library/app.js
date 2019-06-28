31e7bb24a0e29e90bf111b40f6c0f233dc130818
Gabriel Cruz <gabs.oficial98@gmail.com>
2019-06-28 14:35:37 -0300

    Add part 5: displaying data

--- ex17-express-library/app.js
+++ ex17-express-library/app.js
@@ -12,7 +12,7 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://gmelodie:harry@locallibrary-lplq7.azure.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = 'mongodb+srv://gmelodie:harry4@locallibrary-lplq7.azure.mongodb.net/local_library?retryWrites=true&w=majority';
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
